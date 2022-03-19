93cbe2b6624a396c9b72d1e3986b50f08ea3f226
PawalAtakosi <91660424+PawalAtakosi@users.noreply.github.com>
2022-03-19 18:45:03 +0700

    mtr16-19

--- materi16sampai19.js
+++ materi16sampai19.js
@@ -6,6 +6,18 @@ materi16sampai19.listen (3000,function(){
     console.log("Komponen berhasil diubah yayay");
 })
 
-materi16sampai19.get("/test", function(req,res) {
-    res.send("Haihaihaii!!")
+materi16sampai19.get("/materi16", function(req,res) {
+    res.send("Ubah komponen penilaian")
+})
+
+materi16sampai19.get("/materi17", function(req,res) {
+    res.send("Hapus komponen penilaian")
+})
+
+materi16sampai19.get("/materi18", function(req,res) {
+    res.send("Tambah pertemuan mingguan RPS")
+})
+
+materi16sampai19.get("/materi19", function(req,res) {
+    res.send("Ubah pertemuan mingguan yang ada dalam RPS")
 })
