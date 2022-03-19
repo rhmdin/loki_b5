ee17421a7ae78b145dca0d11dec7208d77e90a01
deanfisabilandwi1010 <90377775+deanfisabilandwi1010@users.noreply.github.com>
2022-03-19 18:44:28 +0700

    Materi 6-10 FIxx

--- materi6sampe10.js
+++ materi6sampe10.js
@@ -6,6 +6,22 @@ materi6sampe10.listen (3000,function(){
     console.log("jalan gesss")
 })
 
-materi6sampe10.get("/", function(req,res){
-    res.send("welkam to papji mobail")
+materi6sampe10.get("/materi6", function(req,res){
+    res.send("nambah RPS gais")
+})
+
+materi6sampe10.get("/materi7", function(req,res){
+    res.send("mengubah RPS gais")
+})
+
+materi6sampe10.get("/materi8", function(req,res){
+    res.send("Revisi RPS ni gais")
+})
+
+materi6sampe10.get("/materi9", function(req,res){
+    res.send("Tambah CPMK Kuliah")
+})
+
+materi6sampe10.get("/materi6", function(req,res){
+    res.send("Hapus CPMK Kuliah")
 })
