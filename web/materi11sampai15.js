4995d497187f3d0646089c7040f292b081b4d7c3
Reysha28 <irsyalinareysha@gmail.com>
2022-03-20 00:58:30 +0700

    done

--- web/materi11sampai15.js
+++ web/materi11sampai15.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const materi11sampai15 = express();
 
+materi11sampai15.listen (3000,function(){
+    console.log("Server oke");
+});
+
 materi11sampai15.get("/materi11", function(req ,res) {
     res.send("Pilihan : Menghapus CPMK Mata Kuliah")
 });
@@ -14,13 +18,10 @@ materi11sampai15.get("/materi13", function(request ,response) {
 });
 
 materi11sampai15.get("/materi14", function(request ,response) {
-    response.send("Pilihan : Menghapus referensi !")
+    response.send("Pilihan : Menghapus referensi")
 });
 
 materi11sampai15.get("/materi15", function(request ,response) {
-    response.send("Pilihan : Menambah komponen penilaian !")
+    response.send("Pilihan : Menambah komponen penilaian")
 });
 
-materi11sampai15.listen (3000,function(){
-    console.log("Server oke");
-});
