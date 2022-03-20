b9be1022f6439c14f850ba1c1311fce01fe80984
Reysha28 <irsyalinareysha@gmail.com>
2022-03-20 10:25:27 +0700

    ok

--- web/materi11sampai15.js
+++ web/materi11sampai15.js
@@ -5,31 +5,43 @@ materi11sampai15.listen (3000,function(){
     console.log("Server okey");
 });
 
-materi11sampai15.delete("/deletecpmk", function(req ,res) {
+materi11sampai15.get("/deletecpmk", function(req ,res) {
     res.send("Pilihan : Menghapus CPMK Mata Kuliah")
 });
 
-materi11sampai15.get("/ref", function(request ,response) {
-    response.send("Daftar referensi")
+materi11sampai15.delete("/deletecpmk", function(req ,res) {
+    res.send("Berhasil enghapus CPMK Mata Kuliah")
 });
 
-materi11sampai15.post("/addref", function(request ,response) {
+materi11sampai15.get("/addref", function(request ,response) {
     response.send("Pilihan : Menambah referensi")
 });
 
-materi11sampai15.put("/updateref", function(request ,response) {
+materi11sampai15.post("/addref", function(request ,response) {
+    response.send("Berhasil menambah referensi")
+});
+
+materi11sampai15.get("/updateref", function(request ,response) {
     response.send("Pilihan : Mengubah referensi")
 });
 
-materi11sampai15.delete("/deleteref", function(request ,response) {
+materi11sampai15.put("/updateref", function(request ,response) {
+    response.send("Berhasil mengubah referensi")
+});
+
+materi11sampai15.get("/deleteref", function(request ,response) {
     response.send("Pilihan : Menghapus referensi")
 });
 
-materi11sampai15.get("/kompnilai", function(request ,response) {
-    response.send("Daftar komponen penilaian")
+materi11sampai15.delete("/deleteref", function(request ,response) {
+    response.send("Berhasil menghapus referensi")
+});
+
+materi11sampai15.get("/addkompnilai", function(request ,response) {
+    response.send("Pilihan : menambah komponen penilaian")
 });
 
 materi11sampai15.post("/addkompnilai", function(request ,response) {
-    response.send("Pilihan : Menambah komponen penilaian")
+    response.send("Berhasil menambah komponen penilaian")
 });
 
