42f735fcd0f88ddad44420e030b87502d1248f5f
deanfisabilandwi1010 <90377775+deanfisabilandwi1010@users.noreply.github.com>
2022-03-20 14:51:01 +0700

    testing server

--- web/materi6sampe10.js
+++ web/materi6sampe10.js
@@ -3,7 +3,7 @@ const { appendFile } = require("fs")
 const materi6sampe10 = express()
 
 materi6sampe10.listen (3000,function(){
-    console.log("jalan gesss")
+    console.log("Berhasil Terkoneksi Ke Server")
 })
 
 materi6sampe10.get("/tambahRPS", function(req,res){
@@ -22,6 +22,6 @@ materi6sampe10.get("/TambahCPMK", function(req,res){
     res.send("Tambah CPMK Kuliah")
 })
 
-materi6sampe10.get("/HapusCPMK", function(req,res){
+materi6sampe10.delete("/HapusCPMK", function(req,res){
     res.send("Hapus CPMK Kuliah")
 })
