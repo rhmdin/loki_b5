ce8d3f56801ca1030ecda77aba1db9d03fe846d7
Rizki Juni Darmawan <90382451+riskikii@users.noreply.github.com>
2022-03-20 14:19:54 +0700

    done

--- web/fungsi21sampai23.js
+++ web/fungsi21sampai23.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const fungsi21sampai23 = express();
-const pdf = require('html-pdf');
 
 fungsi21sampai23.listen (3000,function(){
     console.log("Server Ready");
 });
 
-fungsi21sampai23.get("/materi6", function(req,res){
+fungsi21sampai23.get("/search", function(req,res){
     res.send("Data berhasil ditampilkan")
+})
+
+fungsi21sampai23.get("/detail", function(req,res){
+    res.send("Detail RPS")
+})
+
+fungsi21sampai23.get("/generate-pdf", function(req,res){
+    res.send("Generate Berhasil")
 })
 
   
