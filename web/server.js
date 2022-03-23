3f5ad95d125070ea8e13c5a33c2f0399616a6ffb
Reysha28 <irsyalinareysha@gmail.com>
2022-03-23 11:08:50 +0700

    done

--- web/server.js
+++ web/server.js
@@ -4,6 +4,7 @@ const server = express();
 server.get("/", (req, res) => {
     res.send("Server is connected")
   })
+  
 server.listen (3000,function(){
     console.log("Alhamdulillah bisaa ges dah nyambung")
 })
@@ -24,6 +25,36 @@ server.get("/userlist", function(req,res) {
 server.get("/rpslist", function(req,res) {
     res.send("Daftar RPS")
 })
+
 server.get("/printrps", function(req,res) {
     res.send("Cetak RPS")
 })
+
+//Fungsi 11 : Menghapus CMPK
+server.delete("/cpmk/:id", function(req ,res) {
+    const ID = request.params.id;
+    res.send('Berhasil menghapus CPMK Mata Kuliah id: ${ID}')
+});
+
+//Fungsi 12 : Menambah referensi
+server.post("/referensi/", function(req ,res){
+    res.send("Berhasil menambah referensi")
+});
+
+//Fungsi 13 : Mengubah referensi
+server.put("/referensi/:id", function(req ,res) {
+    const ID = request.params.id;
+    res.send('Berhasil mengubah referensi id: ${ID}')
+});
+
+//Fungsi 14 : Menghapus referensi
+server.delete("/referensi/:id", function(req ,res) {
+    const ID = request.params.id;
+    res.send('Berhasil menghapus referensi id: ${ID}')
+});
+
+//Fungsi 15 : Menambah komponen penilaian
+server.post("/kompnilai/", function(req ,res) {
+    res.send("Berhasil menambah komponen penilaian")
+});
+
