1585d416244b5de8c09e41d0a3d01b9e6184e70e
deanfisabilandwi1010 <90377775+deanfisabilandwi1010@users.noreply.github.com>
2022-03-27 14:22:07 +0700

    dean materi 6-8

--- web/server.js
+++ web/server.js
@@ -36,15 +36,62 @@ server.get("/tambahRPS", function(req,res){
     res.send("nambah RPS gais")
 })
 
+server.post("/tambahRPS", (req, res) => {
+    let add = {
+      message: "Silahkan tambahkan RPS disini",
+      "Mata Kuliah :": {
+        "kode matkul": "JSI62125",
+        "nama matkul": "Pemrograman Web",
+        "jumlah sks": "3",
+        semester: "4",
+      },
+    };
+    res.json(add);
+    console.log("RPS berhasil ditambahkan");
+  });
+
 //Fungsi 7 Mengubah RPS
 server.get("/UbahRPS", function(req,res){
     res.send("mengubah RPS gais")
 })
 
+//route mengubah RPS yang sudah ada (Materi 7 = Dean Fisabil Andwi)
+server.post("/UbahRPS", (req, res) => {
+    let add = {
+      message: "Silahkan Ubah RPS disini",
+      "Mata Kuliah :": {
+        "kode matkul": "JSI62126",
+        "nama matkul": "Pemrograman Web",
+        "jumlah sks": "2",
+        semester: "4",
+      },
+    };
+    res.json(add);
+    console.log("RPS berhasil diubah");
+  });
+
+
+
 //Fungsi 8 Revisi RPS
 server.get("/RevisiRPS", function(req,res){
     res.send("Revisi RPS ni gais")
 })
+
+//route revisi RPS yang sudah ada (Materi 8 = Dean Fisabil Andwi)
+server.post("/RevisiRPS", (req, res) => {
+    let add = {
+      message: "Silahkan revisi RPS disini",
+      "Mata Kuliah :": {
+        "kode matkul": "JSI62126",
+        "nama matkul": "Pemrograman Web Lanjutan",
+        "jumlah sks": "2",
+        semester: "5",
+      },
+    };
+    res.json(add);
+    console.log("RPS berhasil direvisi");
+  });
+
 
 //Fungsi 9 Tambah CPMK Kuliah
 server.get("/TambahCPMK", function(req,res){
