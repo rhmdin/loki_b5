66d000991da74d897bd22d143e4edaeec3759fae
2012 Rahmadina <89837465+rhmdin@users.noreply.github.com>
2022-03-28 10:04:52 +0700

    Update server.js

--- web/server.js
+++ web/server.js
@@ -9,26 +9,6 @@ server.listen (3000,function(){
     console.log("Alhamdulillah bisaa ges dah nyambung")
 })
 
-//2011522012 Rahmadina Fungsi 1-5
-server.get("/login", function(req,res) {
-    res.send("Log in user")
-})
-
-server.get("/logout", function(req,res) {
-    res.send("Log out user")
-})
-
-server.get("/userlist", function(req,res) {
-    res.send("Daftar Pengguna")
-})
-
-server.get("/rpslist", function(req,res) {
-    res.send("Daftar RPS")
-})
-
-server.get("/printrps", function(req,res) {
-    res.send("Cetak RPS")
-})
 
 //Dean Fisabil Andwi
 //Fungsi 6 Menambah RPS
